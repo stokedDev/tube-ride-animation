3880a381a05e15a575db400afadbeb17ccd49e09
Maurice Rogers <scsurfermaurice@gmail.com>
2023-12-02 21:05:30 -0800

    Refactor JS

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 const images = document.querySelectorAll('img');
-const oneSec = 1000;
 let frame = 0;
 let previousFrame = 0;
 images.forEach(el => {
     el.style.display = 'none';
 });
 setInterval(()=>{
-    previousFrame = frame - 1;
     if(frame === null || frame === 4){
         frame = 0;
-    } else if(frame >= 0 && frame < 4){
+    } else if(frame < 4){
         frame++;
     }
+    previousFrame = frame - 1;
     images[frame].style.display = 'block';
-    images[previousFrame].style.display = "none";
+    images[previousFrame].style.display = 'none';
 },45);
